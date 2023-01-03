2040e996781f345eb4d205b5af366310fde177a2
kylych <kylycsydykov@gmail.com>
2023-01-03 15:45:30 +0600

    added Areases for database

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, Regions
+from .models import User, Regions, Areas
 
 admin.site.register(User)
 admin.site.register(Regions)
+admin.site.register(Areas)
--- main/models.py
+++ main/models.py
@@ -20,3 +20,19 @@ class Regions(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Region'
+        verbose_name_plural = 'Regions'
+
+
+class Areas(models.Model):
+    name = models.CharField(max_length=60)
+    info = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Areas'
+        verbose_name_plural = 'Areases'
